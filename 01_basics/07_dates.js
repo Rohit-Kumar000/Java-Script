96920c11c0d0f6cec00989ebf1d3032c8782a607
Rohit kumar <rohitk50798@gmail.com>
2026-09-14 16:23:03 +0530

    Add 07_dates.js

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,40 @@
+// Dates
+
+let myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toTimeString());
+
+// console.log(typeof myDate);         // object
+
+// let myCreatedDate = new Date(2026,0,24);
+// let myCreatedDate = new Date(2026,0,24,6,9);
+
+// console.log(myCreatedDate.toDateString());
+// console.log(myCreatedDate.toLocaleString());
+
+// let myCreatedDate = new Date("2026-01-24");
+// console.log(myCreatedDate.toLocaleString());
+// console.log(myCreatedDate.toDateString());
+
+// let myCreatedDate = new Date("01-14-2026");
+// console.log(myCreatedDate.toDateString());
+
+let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date();
+// console.log(newDate.getDate());
+// console.log(newDate.getDay());
+// console.log(newDate.getHours());
+
+// newDate.toLocaleString('default' , {
+//     weekday: "long",
+// });
