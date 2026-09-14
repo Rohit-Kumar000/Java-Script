7a0839c3f160a25db2f817d36da9f399fb275d9d
Rohit kumar <rohitk50798@gmail.com>
2026-09-14 11:02:48 +0530

    Add 02_datatypes.js with initial data type examples and console logs

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict";  // treat all JS code as newer version
+
+// alert(3+3); // we are using node.js , not browser
+
+// console.log(3+3) console.log("Rohit");      // code readability should be high
+
+let name = "Rohit";
+let age = 22;
+let isloggedIn = false;
+let state = null;
+let city;
+
+// string => ""
+// number => 2 to power 53
+// bigint
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+//object
+
+console.log(typeof null);       // object
+console.log(typeof undefined);  // undefined
+
