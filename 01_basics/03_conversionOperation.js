ad6838fe637b6c0df37535cc5c263d196eb17668
Rohit kumar <rohitk50798@gmail.com>
2026-09-14 11:23:00 +0530

    Add 03_conversionOperation.js with type conversion examples and console logs

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "Rohit";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "333abc" => NaN  (Not a Number)
+// true => 1; false => 0
+
+let isloggedIn = null;
+let booleanIsLoggedIn = Boolean(isloggedIn);
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Rohit" => true
+// undefined => false
+// null => false
+
+let sum = 45;
+
+let stringNumber = String(sum);
+console.log(typeof stringNumber);
+console.log(stringNumber);
